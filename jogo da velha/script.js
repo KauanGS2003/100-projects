59d3d12eb9780c676c03374c9036e7f7eca0f56b
KauanGS2003 <129869187+KauanGS2003@users.noreply.github.com>
2023-11-23 23:58:41 -0300

    Update script.js

--- jogo da velha/script.js
+++ jogo da velha/script.js
@@ -1,10 +1,13 @@
-let jogo = true
-let jogador = 'X';
+let player = "X";
+let player2 = "O";
+let box = document.getElementsByClassName("box")
+let turn = 0;
+let game = true
 
-function jogar(box) {
-    if (jogo && document.getElementById(box).innerHTML) {
-      document.getElementById(box).innerHTML = jogador;
-      verificarVitoria();
-      jogador = (jogador === 'X') ? 'O' : 'X';
-    }
+function caixa(numeros) {
+  if (game && !box.innerHTML) {
+    player = box.innerHTML;
+
+      player = (player === 'x') ? 'X' : 'O';
+      }
   }
